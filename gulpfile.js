842235b7e38ca2167f654bc3ce87ee48d6c48b14
KeepPassionAlways <406142765@qq.com>
2018-11-28 17:06:35 +0800

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 gulp.task('compass', function () {
-    gulp.src('./js/*.js')  //获取文件，同时过滤掉.min.js文件
+    gulp.src('js/*.js')  //获取文件，同时过滤掉.min.js文件
         .pipe(babel())
         .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('dist/'));  //输出文件
 });
 
